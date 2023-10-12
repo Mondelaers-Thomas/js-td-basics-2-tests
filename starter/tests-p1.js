b8ac0d95198f23930a3430b2bdb741c9d68374fd
Mondelaers Thomas <mondelaers.thomas@gmail.com>
2023-10-12 14:06:19 +0200

    starter fait jusqu'au tests-p2-restosDuCoeur.js.js

--- starter/tests-p1.js
+++ starter/tests-p1.js
@@ -3,3 +3,15 @@ TESTS - PRÉPA 1
 Ecrire un programme JS qui calcule la facture d’un client qui achète N articles vendus au prix x
 sachant qu’on accorde 10% de ristourne si le client achète au moins 20 articles.
 */
+const prixArticle = 10;
+const nombreArticle = Number(prompt("Combien d'article voulez-vous acheter ?"));
+const reduction = 0.1; //(10% ristourne)
+let prixTotal;
+
+if(nombreArticle >= 20){
+    prixTotal = (nombreArticle * prixArticle);
+    prixTotal = prixTotal - (prixTotal * reduction);
+} else {
+    prixTotal = nombreArticle * prixArticle;
+}
+console.log(`Le prix total est de : ${prixTotal}`);
--- starter/tests-p2-restosDuCoeur.js
+++ starter/tests-p2-restosDuCoeur.js
@@ -6,6 +6,31 @@ avec un maximum de 2€.
 Ecrire le programme JS qui, à partir du montant des achats,
 calcule et affiche la somme versée aux restos du cœur.
 */
+let prixPaye = 1000;
+let remiseClient;
+let montantVerse;
+const reduction = 0.1;
+
+if(prixPaye >= 25){
+    remiseClient = prixPaye*reduction;
+    prixPaye = prixPaye - remiseClient;
+    if(remiseClient >= 5){
+        montantVerse = remiseClient * reduction;
+        if(montantVerse <= 2){
+            console.log(`Le montant à payer est de ${prixPaye}€`);
+            console.log(`Le montant versé aux restos du cœur est de ${montantVerse}€`);
+        } else {
+            console.log(`Le montant à payer est de ${prixPaye}€`);
+            console.log("Le montant versé aux restos du cœur est de 2€")
+        }
+    } else {
+        console.log(`Le montant à payer est de ${prixPaye}€`);
+        console.log("Le montant n'est pas assez élevé pour faire de versement aux restos du cœur :(");
+    }
+} else {
+    console.log(`Le montant à payer est de ${prixPaye}€`);
+    console.log("Le montant n'est pas assez élevé pour faire de versement aux restos du cœur :(");
+}
 
 /* Tester avec des montants d'achat de
 - 20€ (pas de remise, pas de montant versé)
